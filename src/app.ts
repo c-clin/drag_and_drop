e6bdcf4988dd4a22498c64ce867365a8e2bd0ce4
Catherine Lin <catherine.lin.93@gmail.com>
2020-12-11 21:59:40 -0800

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+
+	constructor() {
+		this.templateElement = document.getElementById(
+			'project-input'
+		)! as HTMLTemplateElement;
+		this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+		const importNode = document.importNode(this.templateElement.content, true);
+	}
+}
